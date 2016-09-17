cf31d7c25f8812a2450377e464cb03b3f9633273
Chuck Daniels <michaelwolski2010@gmail.com>
2016-09-17 20:53:24 +0200

    indexed data motherfucker!

--- flight-indexer/airport-indexer.py
+++ flight-indexer/airport-indexer.py
@@ -24,8 +24,8 @@ IDX_START = 1
 IDX_JUMP = 85
 
 CSV_FILE_NAME = "flights.csv"
-
-CSV_FILE_COUNTRIES = "country_codes_population.csv"
+CSV_FILE_COUNTRIES = "big-countries.csv"
+# CSV_FILE_COUNTRIES = "country_codes_population.csv"
 def main():
     with open(CSV_FILE_COUNTRIES) as flightsCSV:
         reader = csv.reader(flightsCSV, delimiter=';')
