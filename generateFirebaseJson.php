3301ca1db24a1e5d9324790d58326013f7c4c797
christopher-siegel <c@siegel-march.de>
2016-09-18 05:10:27 +0200

    Firebase anpassungen, r_output anpassung, index mit ZOOOOOM. 5 Uhr morgen cO

--- generateFirebaseJson.php
+++ generateFirebaseJson.php
@@ -1,13 +1,13 @@
 <?php
 $arr = array();
-if (($handle = fopen("magic/R_output.csv", "r")) !== FALSE) {
+if (($handle = fopen("magic/R_output2.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, " ")) !== FALSE) {
         // $data[1] // country
         // $data[2] // week
         // $data[4] // percent
         if ($data[2] != "week") {
-            $arr["y2016w" . $data[2]][$data[1]]["infected"] = $data[3];
-            $arr["y2016w" . $data[2]][$data[1]]["percent"] = $data[4];
+            $arr["y2016w" . $data[2]][$data[5]]["infected"] = $data[3];
+            $arr["y2016w" . $data[2]][$data[5]]["percent"] = $data[4];
         }
     }
     fclose($handle);
